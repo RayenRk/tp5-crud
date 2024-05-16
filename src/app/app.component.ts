a020b925e2481c23a49fa646ac2b615a7004de0e
RayenRk <rakkadrayen@gmail.com>
2024-05-16 17:30:36 +0100

    routes config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from "./post/index/index.component";
+import { ViewComponent } from "./post/view/view.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, IndexComponent, ViewComponent]
 })
 export class AppComponent {
   title = 'tp5-crud';
